36c940e84daf33b7831818664635c1369765acc5
Keith Young <keithy822@gmail.com>
2025-03-11 12:17:53 -0400

    start of project

---
+++ Desktop/personal_projects/website/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+db_name = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "hjvhjvhjhdsjhg hhbhdgabfybcvbcnbnyubv"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix= '/')
+    app.register_blueprint(auth, url_prefix= '/')
+    return app
